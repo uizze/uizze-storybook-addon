b73d738c1c266da918c7b703b56e13b8fce5200a
Samuel Bushi <contact@samuelbushi.com>
2026-07-22 17:07:16 +0200

    Focus the live demo on the UIZZE finish gate

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config = defineMain({
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-docs', fileURLToPath(new URL('./local-preset.ts', import.meta.url))],
   framework: '@storybook/react-vite',
+  managerHead: (head) => `${head}\n<title>UIZZE Finish Gate</title>`,
 });
 
 export default config;
